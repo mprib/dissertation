e87083af7ab3bd0492bfccc39e700831760fddc2
mprib <31831778+mprib@users.noreply.github.com>
2024-03-30 13:27:45 -0500

    Power analysis basically done...need 30 primarily do see interaction in Aim2

--- power_analysis_simulation.R
+++ power_analysis_simulation.R
@@ -4,7 +4,6 @@ library(emmeans)
 
 get_simulated_data <- function(n,
                                conditions,
-                               variable_name, 
                                pre_means, 
                                pre_vars,
                                post_means,
@@ -48,46 +47,12 @@ get_simulated_data <- function(n,
 
 }
 
-conditions <- list( "cSBT", "FastProp", "FastBrake" )
 
-# Aim 2
+simulate_aim <- function(n, conditions, pre_means, pre_vars, post_means, post_vars){
 
-simulate_Aim2_SLR <- function(n){
-    ## H1: SLR increases in each condition (powered to observe differences in effect size from cSBT)
-    variable_name = "SLR"
-    
-    # healthy population...anticipate symmetry
-    pre_means <-list(
-      "cSBT" = 1,
-      "FastProp" = 1,
-      "FastBrake" = 1
-    )
-    
-    post_means <-list(
-      "cSBT" = 1.4, # from lauziere et al 2014
-      "FastProp" = 1.2, # expect half impact
-      "FastBrake" = 1.2 # expect half impact
-    )
-    
-    # from Lauziere et al 2014
-    pre_vars <-list(
-      "cSBT" = .0036,
-      "FastProp" = .0036,
-      "FastBrake" = .0036
-    )
-    
-    # from Lauzier et al 2014
-    # makes sense as greater variance expected after adaptation
-    post_vars <-list(
-      "cSBT" = .07,
-      "FastProp" = .07,
-      "FastBrake" = .07
-    )
-    
     
     simulated_data = get_simulated_data(n, 
                        conditions = conditions,
-                       variable_name = variable_name, 
                        pre_means=pre_means, 
                        pre_vars=pre_vars,
                        post_means = post_means, 
@@ -95,19 +60,21 @@ simulate_Aim2_SLR <- function(n){
     
     model <-  aov(observation ~ condition * period  , data = simulated_data)
     
-    ## ugly but this gets the results of the F test on the interaction
+    # ugly but this gets the results of the F test on the interaction
+    # powering the study to identify differences in impact of condition on 
+    # changes across time periods
     interaction_effect_p_value <- summary(model)[[1]]$'Pr(>F)'[3]
     
     
-    emm <- emmeans(model, ~condition*period)
     # Planned comparison: pre vs. post within each condition
+    emm <- emmeans(model, ~condition*period)
     planned_comparisons <- contrast(emm, "pairwise", by = "condition") %>% 
                            summary(adjust="bonferroni")
     
-    H1_success = all(planned_comparisons$p.value<.05)
+    planned_comparison_success = all(planned_comparisons$p.value<.05)
     
     # do the check of the hypothesis
-    if (all(interaction_effect_p_value<.05, H1_success)){
+    if (all(interaction_effect_p_value<.05,  planned_comparison_success)){
       success <- TRUE
     } else {
       success <-FALSE
@@ -115,24 +82,4 @@ simulate_Aim2_SLR <- function(n){
     return(success)
 
 }
-
-# Mac, start here and build out iterations on this to keep incrementing success rate until
-# it is > .95
-success_count = 0
-success_rate = 0
-n = 20
-for (i in 1:20) {
-  success = simulate_Aim2_SLR(n)
-  if (success){
-    success_count = success_count + 1
-    success_rate = success_count / i
-    print(paste("For n = ", n, "Success rate is ", success_rate, " on ", i, "runs"))
-  }
-   
-}
-
-
-
-## H2: Paretic PF Impulses move in different directions in FastBrake and FastProp
-## Basically: interaction effect for FastBrake and FastProp and significant Difference
 
